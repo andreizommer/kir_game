98289a2c2205604e3ac1b30e38d05ebb90fbe325
andreizommer <228zommerandrei@gmail.com>
2021-09-27 10:44:56 +0700

    code

---
+++ src/main.kt
@@ -0,0 +1,9 @@
+import kotlin.math.atan2
+
+fun main() {
+
+    println( "Begin1. дана сторона квадрата a. найди его площадь S=a*a")
+    print(  "Введите сторону квадрата a*a")
+    var a = readLine()!!.toInt()
+    var S = a*a
+    print("Ответ: Площадь равна $S ")}
